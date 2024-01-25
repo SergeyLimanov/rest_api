7806289a1cee451d365373b31b31aac7d3d8d86c
s.limanov <imajing2010@yandex.ru>
2024-01-25 11:20:14 +0400

    remove import

--- src/main/java/com/limanov/rest/service/KafkaProducer.java
+++ src/main/java/com/limanov/rest/service/KafkaProducer.java
@@ -1,7 +1,6 @@
 package com.limanov.rest.service;
 
 import com.limanov.rest.dto.DogDto;
-import com.limanov.rest.mapper.DogMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
